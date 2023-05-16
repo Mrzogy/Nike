8532e0570669572dd0c51130e413e2eb17a1acb1
mrzogy <magbolmrzogy@gmail.com>
2023-05-16 13:48:42 +0300

    Initial Commit

---
+++ Nike/NikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeApp.swift
+//  Nike
+//
+//  Created by 3bood on 26/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
